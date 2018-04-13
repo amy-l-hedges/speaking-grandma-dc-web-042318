e5d48a710f261de4879517576011bb44738af769
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-13 19:46:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(sentence)
   if sentence == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif sentence == sentence.upcase
+  elsif sentence == sentence.upcase()
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
